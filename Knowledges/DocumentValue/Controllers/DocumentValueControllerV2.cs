b51d9053fa8fa12dfc5b32f3ab167a7c0f9b9e5e
Aynur <mingalik@gmail.com>
2021-12-13 09:31:54 +0300

    add mediatr controller

---
+++ Knowledges/DocumentValue/Controllers/DocumentValueControllerV2.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentWebApi.Controllers
+{
+    public class DocumentValueControllerV2 : Controller
+    {
+        private readonly IMediator mediator;
+
+        public DocumentValueControllerV2(IMediator mediator) => mediator = mediator;
+
+        // POST: DocumentControllerV2/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                await mediator.Send(, HttpContext.RequestAborted);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
